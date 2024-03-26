e27bf878c022ae959c5a6d56a4b1938831ed9d3a
zetdou <zyziupoz@gmail.com>
2024-03-26 18:39:19 +0100

    task 08, 09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,41 @@
+// const completeForm = document.getElementsByClassName('.login-form');
+// const completeInputs = document.getElementsByTagName('input');
 
+// const validateInputs = () => {
+//     const inputRequired = completeInputs.value;
+//     if (inputRequired === 0) {
+//         alert("Musisz wypełnić wszystkie wymagane pola!");
+//     }
+//     else {
+        
+//     }
+// }
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {};
+    for (const input of form.elements) {
+        if (input.tagName === 'INPUT') {
+            formData[input.name] = input.value;
+        }
+    }
+
+    let allFieldsFilled = true;
+    for (const input of form.elements) {
+        if (input.tagName === 'INPUT' && !input.value.trim()) {
+            allFieldsFilled = false;
+            break;
+        }
+    }
+
+    if (!allFieldsFilled) {
+        alert("Musisz wypełnić wszystkie wymagane pola!");
+        return;
+    }
+
+    console.log(formData);
+    form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const changingColor = document.querySelector('.change-color'); 
+let valueOfColor = document.querySelector('.color');
+
+changingColor.addEventListener("click", function(event) {
+  const colorChange = getRandomHexColor();
+  document.body.style.backgroundColor = colorChange;
+  valueOfColor.textContent = colorChange;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
